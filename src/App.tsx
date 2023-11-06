749f2f8f771748c6372ee6ffbd23defbc51521fa
Emily Stinger <eshart@gmail.com>
2023-11-06 14:39:32 -0600

    firebase helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { FC, useState, useEffect } from 'react';
+import { FC } from 'react';
 import './style.css';
 import { Map } from './mapbox/Map';
 import css from './App.module.css'
-import { fetchTeam } from './data';
+import { TeamList } from './ExampleTeam';
 
 export const App: FC<{ name: string }> = ({ name }) => {
-  const [team, setTeam] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetchTeam();
-      setTeam(data);
-    }
-    fetchData();
-  }, []);
   return (
     <div className={css.mapContainer}>
       <Map />
-      {team && (
-        <ul>
-          {team.map(member => <li key={member}>{member}</li>)}
-        </ul>
-      )}
+      <TeamList />
     </div>
   );
 };
