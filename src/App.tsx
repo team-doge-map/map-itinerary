dc093e9054e04ae8a90d93a3766e6fd1ad6f5419
Jason More <Jason.More@gmail.com>
2023-11-06 10:46:07 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import './style.css';
+
+export const App: FC<{ name: string }> = ({ name }) => {
+  // hello world
+  // hi there
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <span>this is pb</span>
+      <p>this is emily</p>
+      <span>do you see this??? what if i make an edit?</span>
+      <p>this is kayti</p>
+    </div>
+  );
+};
