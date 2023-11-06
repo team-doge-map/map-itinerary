6daee8b79ea3f013808a63a7ae6dbd1ab683121f
Emily Stinger <emily.stinger@thrivent.com>
2023-11-06 13:46:28 -0600

    firebase shenanigans

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import './style.css';
 import { Map } from './mapbox/Map';
 import css from './App.module.css'
+import { fetchTeam } from './data';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  const [team, setTeam] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchTeam();
+      setTeam(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className={css.mapContainer}>
       <Map />
+      {team && (
+        <ul>
+          {team.map(member => <li key={member}>{member}</li>)}
+        </ul>
+      )}
     </div>
   );
 };
