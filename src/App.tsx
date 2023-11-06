77b29d6a4584c7ffa90237cd281b756ccdb19830
Jason More <Jason.More@gmail.com>
2023-11-06 13:21:45 -0600

    css-modules (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import './style.css';
 import { Map } from './mapbox/Map';
+import css from './App.module.css'
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
-    <div style={{ display: 'flex'}}>
+    <div style={css.mapContainer}>
       <Map />
     </div>
   );
