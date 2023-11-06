b85a27f1f0ac1f8bb1887f60fcb4d04496ccdddb
JasonMore <jason.more@gmail.com>
2023-11-06 11:56:11 -0600

    css-modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 
 import './style.css';
+import css from './App.module.css'
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello {name}!</h1>
+      <h1 className={css.hello}>Hello {name}!</h1>
       <p>Start editing to see some magic happen :)</p>
       <span>this is pb</span>
       <p>this is emily</p>
