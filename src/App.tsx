dcd95ea2cc44e737bbd8f2d36a0a8a33c24a098e
pbrejcha <peter.brejcha@gmail.com>
2023-11-06 11:05:26 -0600

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { FC } from 'react';
-
 import './style.css';
+import { Map } from './mapbox/Map';
 
 export const App: FC<{ name: string }> = ({ name }) => {
-  // hello world
-  // hi there
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <span>this is pb</span>
-      <p>this is emily</p>
-      <span>do you see this??? what if i make an edit?</span>
-      <p>this is kayti</p>
+      <Map />
     </div>
   );
 };
