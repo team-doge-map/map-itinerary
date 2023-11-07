884834e9302561a98a7a2b0d3adb7a3cf505e368
pbrejcha <peter.brejcha@gmail.com>
2023-11-07 10:59:32 -0600

    mapbox thing

--- src/mapbox/Map.tsx
+++ src/mapbox/Map.tsx
@@ -14,20 +14,105 @@ export const Map = () => {
     if (map.current) return;
 
     map.current = new mapboxgl.Map({
+      attributionControl: false,
       container: mapContainer.current || '',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [location.lat, location.lng], // TODO: these values should probably come from global state or context
       zoom: 6
     });
+
+    map.current.on('load', () => {
+      map.current.loadImage(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+        (error, image) => {
+          if (error) throw error;
+          map.current.addImage('custom-marker', image);
+          // Add a GeoJSON source with 2 points
+          map.current.addSource('points', {
+            'type': 'geojson',
+            'data': {
+              'type': 'FeatureCollection',
+              'features': [
+                {
+                  // feature for Mapbox DC
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [
+                      -77.03238901390978, 38.913188059745586
+                    ]
+                  },
+                  'properties': {
+                    'title': 'Mapbox DC'
+                  }
+                },
+                {
+                  // feature for Mapbox SF
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-122.414, 37.776]
+                  },
+                  'properties': {
+                    'title': 'Mapbox SF'
+                  }
+                }
+              ]
+            }
+          });
+
+          // Add a symbol layer
+          map.current.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'points',
+            'layout': {
+              'icon-image': 'custom-marker',
+              // get the title name from the source's "title" property
+              'text-field': ['get', 'title'],
+              'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+              ],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top'
+            }
+          });
+        }
+      );
+    });
   }, [])
 
   useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
+    if (!map.current || !location) return; // wait for map to initialize
 
     // https://docs.mapbox.com/mapbox-gl-js/api/map/#flyto-parameters
     map.current.flyTo({
       center: [location.lat, location.lng],
     });
+
+    // map.current.addSource('current-location', {
+    //   'type': 'geojson',
+    //   'data': {
+    //     'type': 'FeatureCollection',
+    //     'features': [
+    //       {
+    //         // feature for Mapbox DC
+    //         'type': 'Feature',
+    //         'geometry': {
+    //           'type': 'Point',
+    //           'coordinates': [
+    //             location.lat, location.lng
+    //           ]
+    //         },
+    //       },
+    //     ]
+    //   }
+    // });
+
+    // return () => {
+    //   map.current?.removeSource('current-location');
+    // }
   }, [location]);
 
   return <div ref={mapContainer} className={styles.mapContainer} />
