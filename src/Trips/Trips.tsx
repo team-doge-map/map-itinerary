8615bd16c4f1034625307c6b3b1f94ca0a8bd694
pbrejcha <peter.brejcha@gmail.com>
2023-11-08 14:39:58 -0600

    fixing broken markers

--- src/Trips/Trips.tsx
+++ src/Trips/Trips.tsx
@@ -1,7 +1,11 @@
+import { useTrip } from "../data/useTrip";
 import { FloatingPanel } from "../shared/FloatingPanel/FloatingPanel";
 import { Link } from "react-router-dom";
 
 export const Trips = () => {
+  // just grab first trip for right now.
+  const [trip, tripLoading] = useTrip("trip1");
+
   return (
     <>
       <FloatingPanel>
--- src/data/TripEventAtom.ts
+++ src/data/TripEventAtom.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 import { EventLocations } from "./mock/mockData";
 
 export const tripEventAtom = atom<EventLocations | null>(null);
+export const eventLocationsAtom = atom<EventLocations[]>([]);
